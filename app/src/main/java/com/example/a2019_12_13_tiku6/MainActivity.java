b2e1c48cf6cf5a9a0d95105dbaccff6428284970
szqq <1184791118@qq.com>
2019-12-31 09:49:21 +0800

    1

---
+++ app/src/main/java/com/example/a2019_12_13_tiku6/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a2019_12_13_tiku6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
