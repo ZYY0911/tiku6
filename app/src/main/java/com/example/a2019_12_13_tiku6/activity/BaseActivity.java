04a93280607a7d05033a4df374520ece1a7a0bca
ZYY <605061405@qq.com>
2019-12-31 09:53:07 +0800

    base

---
+++ app/src/main/java/com/example/a2019_12_13_tiku6/activity/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.example.a2019_12_13_tiku6.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Create by 张瀛煜 on 2019-12-31
+ */
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
